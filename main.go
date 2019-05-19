7720e53d6832232034dfd5d913c93bd8e5abc0e6
tyagian <tyagi.an@husky.neu.edu>
2019-05-19 01:26:29 -0400

    added 404 page if page not found

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1> Home Page!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send email to <a href=\"mailto:support@abc.com\">support@abc.com</a>.")
+		//fmt.Fprint(w, "To get in touch, please contact <a href=\"mailto:support@lenslocked.com\">support@abc.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> We could not find the page you are looking for. Sorry :( </h1>")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+
 }

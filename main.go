a365c028539ace552092de752c41e33271406016
tyagian <tyagi.an@husky.neu.edu>
2019-05-19 18:15:58 -0400

    added contact page

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
+/*
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
@@ -18,17 +19,24 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1> We could not find the page you are looking for. Sorry :( </h1>")
 	}
+} */
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Welcome to my awesome site!</h1>")
 }
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// reason to not use it always because of 3rd arg defined in the function
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send email to <a href=\"mailto:support@abc.com\">support@abc.com</a>.")
+	}
 }
 
 func main() {
-	router := httprouter.New()
-	//router.GET("/hello/:name", Hello)
-	router.GET("/hello/:name/spanish", Hello)
-	//http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	//r.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", r)
 
 }

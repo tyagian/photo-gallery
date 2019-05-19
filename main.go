ad3434a65cccfaaab91a8bac9b65b1ac3d3929f5
tyagian <tyagi.an@husky.neu.edu>
2019-05-19 15:30:23 -0400

    defining func

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +19,16 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1> We could not find the page you are looking for. Sorry :( </h1>")
 	}
 }
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// reason to not use it always because of 3rd arg defined in the function
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := httprouter.New()
+	//router.GET("/hello/:name", Hello)
+	router.GET("/hello/:name/spanish", Hello)
+	//http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", router)
 
 }
